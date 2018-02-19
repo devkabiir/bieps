54c57ebfe937d584eb4c3936f5aa3cfc257c3d75
kabiir <Sid976628@gmail.com>
2018-02-19 22:40:02 +0530

    add tests for BIP39 (english only)

--- test/bips/39_test.dart
+++ test/bips/39_test.dart
@@ -1,29 +1,24 @@
-import 'dart:math';
-import 'dart:typed_data';
-
 import 'package:test/test.dart';
 
 import '../../lib/bieps.dart';
+import '39_test_vectors.dart';
 
 void main() {
-  test('BIP39 generate mnemonic', () {
-    final List<int> elements = new List<int>.generate(16, (int index) {
-      final Random rng = new Random.secure();
-
-      return rng.nextInt(255);
+  group('BIP39 generate mnemonic tests', () {
+    testVectorsForBip39.forEach((List<String> singleTestVector) {
+      test('for intropy: ${singleTestVector[0]}', () {
+        final String generatedMnemonic =
+            BIP39.generateMnemonics(createUint8ListFromHexString(singleTestVector[0])).join(' ');
+        expect(generatedMnemonic, equals(singleTestVector[1]));
+      });
     });
-    final Uint8List initialEntropy = createUint8ListFromHexString('1998bc74c9ec24c6964233c332a86b0b49009765');
-    print(BIP39.generateMnemonics(initialEntropy));
-  }, skip: true);
-  test('BIP39 Seed Test', () {
-    final String testPassphrase = '';
-    final String testMnemonic = 'bullet issue candy captain wheel keep budget recall country slot slot trouble';
-    final String testSeed =
-        '766881710e81d10abf7faa91f4d778659bbd2682462081231b8fcbbbce7afb52ff049039a2adf77ce237504507ab1b906bab55759b65c74d0962008046c25d9a';
-
-    final Uint8List generatedSeed = BIP39.generateSeed(testMnemonic, testPassphrase);
-    final String generatedSeedHex = formatBytesAsHexString(generatedSeed);
-
-    expect(generatedSeedHex, equals(testSeed));
-  }, skip: false);
+  });
+  group('BIP39 generate seed tests', () {
+    testVectorsForBip39.forEach((List<String> singleTestVector) {
+      test('for mnemonic: ${singleTestVector[1]}', () {
+        final String generatedSeed = formatBytesAsHexString(BIP39.generateSeed(singleTestVector[1], 'TREZOR'));
+        expect(generatedSeed, equals(singleTestVector[2]));
+      });
+    });
+  });
 }
