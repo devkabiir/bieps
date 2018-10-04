f69b2fab0ec69d0974263462da3a1923b9e84a0e
devkabiir <dev@kabiir.me>
2018-10-04 23:06:52 +0530

    :construction: update WalletFile

--- lib/src/utils/wallet_file.dart
+++ lib/src/utils/wallet_file.dart
@@ -1,24 +1,30 @@
 library bieps.utils.wallet_file;
 
-import 'helpers.dart';
+class Crypto {}
 
 /// Ethereum wallet file.
 /// This holds the private key, public address and can sign arbitrary data.
 class WalletFile {
-  String _address;
-  Crypto _crypto;
-  String _id;
-  int _version;
+  final String _address;
+  final Crypto _crypto;
+  final String _id;
+  final int _version;
 
+  /// Create a wallet file with given address and version
+  WalletFile(this._address, this._crypto, this._id, this._version);
+
+  @override
+  int get hashCode => _address.hashCode + _version.hashCode;
+
+  @override
+  //ignore: avoid_annotating_with_dynamic
   bool operator ==(dynamic other) {
-    other = getValIfmatch(other, WalletFile);
-
-    if (_address != other._address) {
-      return false;
+    if (other is WalletFile) {
+      if (_address == other._address && _version == other._version) {
+        return true;
+      }
     }
 
-    return true;
+    return false;
   }
 }
-
-class Crypto {}
